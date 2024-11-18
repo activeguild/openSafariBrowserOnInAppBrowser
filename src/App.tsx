48e656e38ea92ce607a73cc7aab895e62799436c
j1ngzoue <workdrive0306@gmail.com>
2024-11-18 12:48:32 +0900

    chore

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import "./App.css";
 function App() {
   return (
     <>
+      <div>{window.navigator.userAgent}</div>
       <button onClick={() => window.open("https://example.com", "_blank")}>
         Open Safari
+      </button>
+      <button onClick={() => window.open("  ://example.com", "_blank")}>
+        Open Chrome(googlechromes)
       </button>
     </>
   );
